35e65017faf95f350f64ae57282ee71e0136ff1f
Victor Lotti <prort818@gmail.com>
2024-07-25 23:03:49 -0300

    Projeto Spring Boot com MongoDB

---
+++ src/main/java/com/project/sbmongodb/ProjetospringmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.project.sbmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospringmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospringmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sbmongodb/ProjetospringmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sbmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetospringmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
