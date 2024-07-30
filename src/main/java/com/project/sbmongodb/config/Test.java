18925ee94748a4f3b56ff574ab3370db27eb385f
Victor Lotti <prort818@gmail.com>
2024-07-29 21:38:56 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/project/sbmongodb/config/Test.java
+++ src/main/java/com/project/sbmongodb/config/Test.java
@@ -1,6 +1,5 @@
 package com.project.sbmongodb.config;
 
-import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -12,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.project.sbmongodb.domain.Post;
 import com.project.sbmongodb.domain.User;
 import com.project.sbmongodb.dto.AuthorDTO;
+import com.project.sbmongodb.dto.CommentDTO;
 import com.project.sbmongodb.repositories.PostRepository;
 import com.project.sbmongodb.repositories.UserRepository;
 
@@ -40,6 +40,14 @@ public class Test implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/06/2000"), "Partiu viagem", "Vou viajar para São Paulo. Abracos!", new AuthorDTO(user1));
 		Post post2 = new Post(null, sdf.parse("24/07/2000"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(user1));
+		
+		CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/06/2000"), new AuthorDTO(user3));
+		CommentDTO comment2 = new CommentDTO("Aproveite a viagem", sdf.parse("21/06/2000"), new AuthorDTO(user2));
+		CommentDTO comment3 = new CommentDTO("Tenha um bom dia!!", sdf.parse("24/07/2000"), new AuthorDTO(user3));
+		
+		
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().addAll(Arrays.asList(comment3));
 		post.saveAll(Arrays.asList(post1, post2));
 		
 		user1.getPosts().addAll(Arrays.asList(post1, post2));
